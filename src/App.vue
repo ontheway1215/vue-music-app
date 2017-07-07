cdad7f26a027698480aa42318c33aaa7d34d0345
chen <179023084@qq.com>
2017-07-07 23:39:27 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scope lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/variable"
+
+  #app {
+    color: $color-theme
+  }
+</style>
